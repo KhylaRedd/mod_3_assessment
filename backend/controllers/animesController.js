21e7172680eea90214673de0426e1b6db99d8453
KhylaRedd <kreddick@pursuit.org>
2024-08-14 13:51:05 -0400

    Final product

--- backend/controllers/animesController.js
+++ backend/controllers/animesController.js
@@ -4,12 +4,15 @@ const {
   getAllAnimes,
   getOneAnime,
   createOneAnime,
-  updateOneAnime,
   deleteOneAnime,
+  updateOneAnime,
 } = require("../queries/animes");
+
 
 /* Instructions: Use the following prompts to write the corresponding routes. **Each** route should be able to catch server-side and user input errors(should they apply). Consult the test files to see how the routes and errors should work.*/
 //Write a GET route that retrieves all animes from the database and sends them to the client with a 200 status code
+
+
 //your response body should look this(ignore the length of the array):
 // [
 //   {
@@ -24,6 +27,32 @@ const {
 //   }
 // ]
 
+animes.get("/", async (req,res)=> {
+  const allAnimes = await
+   getAllAnimes();
+
+   if (allAnimes[0]) {
+        res.status(200).json(allAnimes);
+    } else {
+        res.status(500).json({ error: "Server Error" });
+    }
+});
+
+//get ONE 
+
+animes.get('/:id', async (req, res)=>{
+  const { id } = req.params;
+  const oneAnime = await getOneAnime(id);
+  if(oneAnime.id){
+      res.status(200).json(oneAnime);
+  }else{
+      res.status(404).json({error: "Anime can not be found"});
+  }
+});
+
+
+
+
 //Write a POST route that takes user provided data from the request body and creates a new anime in the database. The route should respond with a 201 status code and the new anime.
 //if the request body does not contain a name and description, or if the body's name or description have no length, respond with an error
 //your response body should look this:
@@ -32,7 +61,18 @@ const {
 //   "name": "test",
 //   "description": "this is anime"
 // }
+animes.post("/", async (req, res) => {
+  try {
+    const { name, description } = req.body;
 
+    const newAnime = await createOneAnime(name, description);
+    
+    res.status(201).json(newAnime);
+  } catch (error) {
+    
+    res.status(500).json({ error: "An error occurred while creating the Anime." });
+  }
+});
 //Write a PUT route that takes user provided data from the request body and updates an existing anime in the database. The route should respond with a 200 and the updated anime. The route should be able to handle a non-existent anime id.
 //if the request body does not contain a name and description, or if the body's name or description have no length, respond with an error
 //your response body should look this:
@@ -41,7 +81,17 @@ const {
 //   "name": "test1",
 //   "description": "this is anime as well"
 // }
-
+animes.put('/:id',async(req,res)=>{
+  const newInfo = req.body;
+  const { id } = req.params;
+  const updateAnimeInfo = await 
+  updateOneAnime(id, newInfo);
+   if(updateAnimeInfo.id){
+        res.status(200).json(updateAnimeInfo);
+}else{
+  res.status(404).json({ error: "Anime Not Found" });
+}
+})
 //Write a DELETE route that deletes a single anime by id (provided by the client as a request param) from the database and responds with a 200 and the deleted anime data. The route should be able to handle a non-existent anime id.
 //your response body should look this:
 // {
@@ -49,4 +99,20 @@ const {
 //   "name": "test1",
 //   "description": "this is anime as well"
 // }
+animes.delete("/:id",async (req,res)=>{
+  const { id }=req.params
+  const deletedAnime = await 
+  deleteOneAnime(id);
+
+  if(deletedAnime.id){
+    res.status(200).json({
+    message: "Anime removed" 
+    })
+  } else {
+    res.status(404).json({error: "Anime not deleted"
+    })
+
+  }
+
+})
 module.exports = animes;
--- frontend/src/Components/Animes.js
+++ frontend/src/Components/Animes.js
@@ -1,13 +1,45 @@
 import Anime from "./Anime";
+import { useState, useEffect } from "react";
+
+
+const API = process.env.REACT_APP_BASE_URL;
 
 function Animes() {
   //fetch all animes from the backend and render them as a list using the Anime component. Make sure to style the animes to look like the screenshot from the README. Feel free to use axios to grab data
+  const [animes,setAnimes]=useState([]);
+  const [error, setError]= useState(null);
+  console.log(animes)
+  useEffect(() => {
+    fetch(`${API}/animes`)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then(res => {
+        if (!Array.isArray(res)) {
+          throw new TypeError("Response is not an array");
+        }
+        setAnimes(res);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError("Failed to fetch Anime data");
+      });
+  }, []);
 
   return (
     <section className="index" id="anime-list">
-      <p>No animes to display</p>
+      {error && <p>{error}</p>}
+      {animes.length === 0 && !error && <p>No animes found</p>}
+      
+      {animes.map((anime) => {
+        const {name, description} = anime
+       return  <Anime key={anime.id} name={name} description={description}  id={anime.id} />
+      })}
     </section>
-  );
-}
+  )
+};
 
 export default Animes;
